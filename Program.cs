4eec3f98a5b12c79282ff92595f3f43d57b0cef8
BBAMO <gao0708@naver.com>
2025-01-16 15:30:51 +0900

    편지, 배열 자르기

--- Program.cs
+++ Program.cs
@@ -2,12 +2,17 @@
 {
     private static void Main(string[] args)
     {
-        //Solution sol = new Solution();
-        //Console.WriteLine(sol.solution01142(64,6));
-        Sample sam = new Sample();
+        Solution sol = new Solution();
+        int[] intarray = new int[]{1,2,3,4,5};
+        //string str = "I love you~";
+        
+        //Console.WriteLine(sol.solution01162(intarray, 1, 3));
+        Util.PrintIntArray(sol.solution01162(intarray, 1, 3));
+
+        //Sample sam = new Sample();
         //sam.Array();
         //sam.RandomSum();
-        sam.List();
+        //sam.List();
     }
     
 }
